7c4b13645e132e5c891958ba526a1cbf359a20d1
Petar Efnushev <petar.efnushev@gmail.com>
2016-07-19 17:42:51 +0200

    Update deals API client

--- hubspotapi/deals.py
+++ hubspotapi/deals.py
@@ -1,3 +1,4 @@
+import json
 from .base import BaseClient
 
 class Deals(BaseClient):
@@ -22,3 +23,26 @@ class Deals(BaseClient):
             path = '/deals/v1/deal/{deal_id}'.format(deal_id=id)
             
         return self.call(method='DELETE', path=path)
+    
+    def get_pipelines(self, path=None):
+        if path is None:
+            path = '/deals/v1/pipelines'
+            
+        return self.call(method='GET', path=path)
+    
+    def get_pipeline_by_id(self, id, path=None):
+        if path is None:
+            path = '/deals/v1/pipelines/{pipeline_id}'.format(pipeline_id=id)
+            
+        return self.call(method='GET', path=path)
+    
+    def get_app_pipeline(self):
+        response = self.get_pipelines()
+        pipelines = json.loads(response.content)
+        
+        for pipeline in pipelines:
+            if 'label' in pipeline:
+                if pipeline.get('label').lower() == self.pipeline.lower():
+                    return pipeline
+                
+        return None

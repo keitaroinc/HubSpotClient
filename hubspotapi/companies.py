6f76187a72b05764d31f5c413e463a8ae415ee1c
Petar Efnushev <petar.efnushev@gmail.com>
2016-07-19 17:42:34 +0200

    Update companies API client

--- hubspotapi/companies.py
+++ hubspotapi/companies.py
@@ -9,7 +9,9 @@ class Companies(BaseClient):
         return self.call(method='GET', path=path)
     
     def create(self, data, path = '/companies/v2/companies'):
-        return self.call(method='POST', data=data, path=path)
+        return self.call(method='POST', data=data, path=path, 
+                         args={'portalId': self.portal_id},
+                         headers={'Accept': 'applicaton/json'})
     
     def update(self, cid, data, path=None):
         if path is None:
@@ -22,3 +24,11 @@ class Companies(BaseClient):
             path = '/companies/v2/companies/{company_id}'.format(company_id=cid)
             
         return self.call(method='DELETE', path=path)
+    
+    def add_contact(self, company_id, contact_id, path=None):
+        if path is None:
+            path = '/companies/v2/companies/{company_id}/contacts/{contact_id}'\
+                    .format(company_id=company_id, contact_id=contact_id)
+            
+        return self.call(method='PUT', path=path)
+        
